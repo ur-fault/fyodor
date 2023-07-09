4080db64cbe6981f6f395e6ab4e26441312a5a9f
ur-fault <minefuf.fufik@gmail.com>
2023-07-09 11:59:51 +0200

    Update: rework Drawable

--- src/drawable.rs
+++ src/drawable.rs
@@ -6,32 +6,29 @@ use crate::canvas::CanvasLike;
 use super::renderer::{Cell, Dims};
 
 pub trait Drawable {
-    fn draw<F>(&self, pos: Dims, frame: &mut F)
-    where
-        F: CanvasLike,
-    {
-        self.draw_styled(pos, frame, ContentStyle::default())
-    }
-    fn draw_styled<F>(&self, pos: Dims, frame: &mut F, style: ContentStyle)
-    where
-        F: CanvasLike;
+    fn draw(&self, pos: Dims, frame: &mut impl CanvasLike);
 }
 
 impl Drawable for char {
-    fn draw_styled<F>(&self, (x, y): Dims, frame: &mut F, style: ContentStyle)
-    where
-        F: CanvasLike,
-    {
+    fn draw(&self, pos: Dims, frame: &mut impl CanvasLike) {
+        (*self, ContentStyle::default()).draw(pos, frame);
+    }
+}
+
+impl Drawable for (char, ContentStyle) {
+    fn draw(&self, (x, y): Dims, frame: &mut impl CanvasLike) {
+        let style = self.1;
+
         if x >= frame.size().0 || y >= frame.size().1 {
             return;
         }
 
-        let width = self.width().unwrap_or(1) as i32;
+        let width = self.0.width().unwrap_or(1) as i32;
         if width == 0 {
             return;
         }
 
-        let cell = Cell::styled(*self, style);
+        let cell = Cell::styled(self.0, style);
 
         frame.set((x, y), cell);
 
@@ -42,78 +39,39 @@ impl Drawable for char {
 }
 
 impl Drawable for String {
-    fn draw<F>(&self, pos: Dims, frame: &mut F)
-    where
-        F: CanvasLike,
-    {
+    fn draw(&self, pos: Dims, frame: &mut impl CanvasLike) {
         self.as_str().draw(pos, frame);
     }
+}
 
-    fn draw_styled<F>(&self, pos: Dims, frame: &mut F, style: ContentStyle)
-    where
-        F: CanvasLike,
-    {
-        self.as_str().draw_styled(pos, frame, style);
+impl Drawable for (String, ContentStyle) {
+    fn draw(&self, pos: Dims, frame: &mut impl CanvasLike) {
+        (self.0.as_str(), self.1).draw((pos.0, pos.1), frame);
     }
 }
 
 impl<'a> Drawable for &'a str {
-    fn draw<F>(&self, pos: Dims, frame: &mut F)
-    where
-        F: CanvasLike,
-    {
+    fn draw(&self, pos: Dims, frame: &mut impl CanvasLike) {
         let mut i = 0;
         for chr in self.chars() {
             chr.draw((pos.0 + i as i32, pos.1), frame);
             i += chr.width().unwrap_or(1) as i32;
         }
     }
+}
 
-    fn draw_styled<F>(&self, pos: Dims, frame: &mut F, style: ContentStyle)
-    where
-        F: CanvasLike,
-    {
+impl<'a> Drawable for (&'a str, ContentStyle) {
+    fn draw(&self, pos: Dims, frame: &mut impl CanvasLike) {
         let mut i = 0;
-        for character in self.chars() {
-            character.draw_styled((pos.0 + i as i32, pos.1), frame, style);
-            i += character.width().unwrap_or(1) as i32;
+        for chr in self.0.chars() {
+            (chr, self.1).draw((pos.0 + i as i32, pos.1), frame);
+            i += chr.width().unwrap_or(1) as i32;
         }
     }
 }
 
 impl Drawable for Cell {
-    fn draw<F>(&self, pos: Dims, frame: &mut F)
-    where
-        F: CanvasLike,
-    {
+    fn draw(&self, pos: Dims, frame: &mut impl CanvasLike) {
         frame.set(pos, *self);
-    }
-
-    fn draw_styled<F>(&self, pos: Dims, frame: &mut F, style: ContentStyle)
-    where
-        F: CanvasLike,
-    {
-        let mut cell = *self;
-        if let Cell::Content(content) = &mut cell {
-            content.style = style;
-        }
-
-        frame.set(pos, cell);
-    }
-}
-
-impl<D: Drawable> Drawable for (D, ContentStyle) {
-    fn draw<F>(&self, pos: Dims, frame: &mut F)
-    where
-        F: CanvasLike,
-    {
-        self.0.draw_styled(pos, frame, self.1);
-    }
-
-    fn draw_styled<F>(&self, pos: Dims, frame: &mut F, style: ContentStyle)
-    where
-        F: CanvasLike,
-    {
-        self.0.draw_styled(pos, frame, style);
     }
 }

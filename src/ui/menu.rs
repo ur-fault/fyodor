5e3869b5e4dedc87c421237b4cf0072af6fc1ae8
ur-fault <minefuf.fufik@gmail.com>
2023-11-30 15:07:33 +0100

    Small changes

---
+++ src/ui/menu.rs
@@ -0,0 +1,48 @@
+use crossterm::style::ContentStyle;
+
+use crate::{
+    canvas::CanvasLike,
+    drawable::Drawable,
+    layout::{sized::Aligned, Pos},
+};
+
+pub struct Menu<T> {
+    title: String,
+    items: Vec<T>,
+    numbered: bool,
+    selected: usize,
+    box_style: ContentStyle,
+    text_style: ContentStyle,
+    selected_style: Option<ContentStyle>,
+}
+
+impl<T> Menu<T> {
+    pub fn new(title: String) -> Self {
+        Self {
+            title,
+            items: Vec::new(),
+            numbered: false,
+            selected: 0,
+            box_style: ContentStyle::default(),
+            text_style: ContentStyle::default(),
+            selected_style: None,
+        }
+    }
+
+    pub fn with_items(mut self, items: Vec<T>) -> Self {
+        self.items = items;
+        self
+    }
+}
+
+impl<T> Drawable for Menu<T>
+where
+    T: Into<String>,
+{
+    type X = Aligned;
+    type Y = Aligned;
+
+    fn draw(self, pos: impl Into<Pos<Self::X, Self::Y>>, frame: &mut impl CanvasLike) {
+        todo!()
+    }
+}
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -2,6 +2,7 @@ use crate::renderer::Renderer;
 
 pub mod popup;
 pub mod fullscreen_popup;
+pub mod menu;
 
 pub trait Window {
     type Output;

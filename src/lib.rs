5b73615c0f72932b70878bf0e148e3d8ad7fe77b
ur-fault <minefuf.fufik@gmail.com>
2023-12-03 09:36:18 +0100

    Add: export main objects in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,10 @@ pub mod helpers;
 pub mod renderer;
 pub mod ui;
 pub mod layout;
+
+pub use canvas::{Canvas, CanvasLike, CanvasLikeExt};
+pub use cell::Cell;
+pub use drawable::Drawable;
+pub use frame::Frame;
+pub use renderer::Renderer;
+pub use layout::Dims;
